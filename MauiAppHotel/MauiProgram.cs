8fc9399c605d66841cb9e389f8267bc8b8bfe6e1
manuellybusinari <manuellybusinari@outlook.com>
2025-05-17 23:17:01 -0300

    Add recursos app Hotel

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,19 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Comfortaa-Bold.ttf", "ComfortaaBold");
+                    fonts.AddFont("Comfortaa-Light.ttf", "ComfortaaLight");
+                    fonts.AddFont("Comfortaa-Medium.ttf", "ComfortaaMedium");
+                    fonts.AddFont("Comfortaa-Regular.ttf", "ComfortaaRegular");
+                    fonts.AddFont("Comfortaa-SemiBold.ttf", "ComfortaaSemiBold");
+                    fonts.AddFont("Comfortaa-Regular.ttf", "ComfortaaRegular");
+                    fonts.AddFont("Oswald-Bold.ttf", "OswaldBold");
+                    fonts.AddFont("Oswald-Medium.ttf", "OswalMedium");
+                    fonts.AddFont("Oswald-Regular.ttf", "OswaldRegular");
+
+
+
+
                 });
 
 #if DEBUG
